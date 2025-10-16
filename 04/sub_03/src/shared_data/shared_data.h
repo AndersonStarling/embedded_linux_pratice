1214dbd3c2d1358c714cf820ba384a86850abf45
AndersonStarling <84923856+AndersonStarling@users.noreply.github.com>
2025-10-16 17:10:25 +0700

    Update shared_data.h

--- 04/sub_03/src/shared_data/shared_data.h
+++ 04/sub_03/src/shared_data/shared_data.h
@@ -1,1 +1,21 @@
+#ifndef SHARED_DATA_H
+#define SHARED_DATA_H
+
+void shared_data_lock(void);
+
+void shared_data_unlock(void);
+
+void shared_data_signal_condition(void);
+
+void shared_data_wait_condition(void);
+
+void shared_data_set_ready_flag(bool ready_flag);
+
+void shared_data_get_ready_flag(void);
+
+void shared_data_update_val(long long data);
+
+long long shared_data_get_val(void);
+
+#endif /* SHARED_DATA_H */
 
