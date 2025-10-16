42d77941a97006153938b5299a90c784f998fee0
AndersonStarling <84923856+AndersonStarling@users.noreply.github.com>
2025-10-16 16:33:53 +0700

    Update shared_data.c

--- 04/sub_03/src/shared_data/shared_data.c
+++ 04/sub_03/src/shared_data/shared_data.c
@@ -3,6 +3,7 @@
 long long shared_data  = 0;
 bool shared_data_ready = false;
 pthread_mutex_t lock;
+pthread_cond_t condition;
 
 void shared_data_lock(void)
 {
@@ -12,6 +13,15 @@ void shared_data_lock(void)
 void shared_data_unlock(void)
 {
     pthread_mutex_unlock(&lock);
+}
+void shared_data_signal_condition(void)
+{
+     pthread_cond_signal(&condition);
+}
+
+void shared_data_wait_condition(void)
+{
+     pthread_cond_wait(&condition, &lock);
 }
 
 void shared_data_set_ready_flag(bool ready_flag)
