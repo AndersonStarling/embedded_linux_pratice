19ffb5c9362336ba8a87848da06c90f444cad082
AndersonStarling <84923856+AndersonStarling@users.noreply.github.com>
2025-10-16 14:12:07 +0700

    Update shared_data.c

--- 04/sub_02/src/shared_data/shared_data.c
+++ 04/sub_02/src/shared_data/shared_data.c
@@ -1,13 +1,11 @@
-#include <stdlib.h>
-
-uint64_t shared_data = 0;
+long long shared_data = 0;
 
 void shared_data_update_val(uint64_t data)
 {
     shared_data = data;
 }
 
-uint64_t shared_data_get_val(void)
+long long shared_data_get_val(void)
 {
     return shared_data;
 }
