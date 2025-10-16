fe0761febbbd2e4dcac14e110738442ba2c8fc33
AndersonStarling <84923856+AndersonStarling@users.noreply.github.com>
2025-10-16 16:18:46 +0700

    Update shared_data.c

--- 04/sub_03/src/shared_data/shared_data.c
+++ 04/sub_03/src/shared_data/shared_data.c
@@ -1,1 +1,24 @@
+#include <pthread.h>
 
+long long shared_data = 0;
+pthread_mutex_t lock;
+
+void shared_data_lock(void)
+{
+     pthread_mutex_lock(&lock);
+}
+
+void shared_data_unlock(void)
+{
+    pthread_mutex_unlock(&lock);
+}
+
+void shared_data_update_val(uint64_t data)
+{
+    shared_data = data;
+}
+
+long long shared_data_get_val(void)
+{
+    return shared_data;
+}
