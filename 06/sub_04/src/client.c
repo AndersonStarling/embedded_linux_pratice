229557bbb95e867e515436ecc3c727ae70143cd4
AndersonStarling <84923856+AndersonStarling@users.noreply.github.com>
2025-10-30 14:13:44 +0700

    Enhance client socket handling with server address
    Refactor socket communication by adding server path and modifying send/receive functions.

--- 06/sub_04/src/client.c
+++ 06/sub_04/src/client.c
@@ -19,6 +19,7 @@ typedef struct
     ssize_t rx_byte_len;
     ssize_t tx_byte_len;
     char rx_buffer[1024];
+    char tx_buffer[1024];
 } common_struct_t;
 
 typedef struct
@@ -29,10 +30,12 @@ typedef struct
     int sock_fd;
 } socket_struct_t;
 
-typedef struct 
+typedef struct
 {
     struct sockaddr_un socket_address;
-    struct sockaddr_un client_address;
+    struct sockaddr_un server_address;
+    char * socket_path;
+    char * server_path;
 } bind_socket_struct_t;
 
 
@@ -49,12 +52,14 @@ socket_struct_t create_socket_data =
     .domain   = AF_UNIX,
     .type     = SOCK_DGRAM,
     .protocol = 0,
-
 };
 
 bind_socket_struct_t bind_socket_data = 
 {
     .socket_address.sun_family = AF_UNIX,
+    .socket_path = "/tmp/client.sock",
+    .server_address.sun_family = AF_UNIX,
+    .server_path = "/tmp/server.sock",
 };
 
 function_sequence sequence[] = 
@@ -69,14 +74,14 @@ function_sequence sequence[] =
         .function = bind_socket,
         .args     = (void *)&bind_socket_data
     },
-    [2] = 
-    {
-        .function = receive_from_socket,
-        .args     = NULL
-    },
-    [3] =
+    [2] =
     {
         .function = send_to_socket,
+        .args     = NULL
+    },
+    [3] = 
+    {
+        .function = receive_from_socket,
         .args     = NULL
     },
     [4] =
@@ -92,6 +97,8 @@ static void create_socket(void * args)
 
     socket_struct_t * socket_data = (socket_struct_t *)args;
 
+    printf("%s\n", __func__);
+
     common_data.socket_fd = socket(socket_data->domain, socket_data->type, socket_data->protocol);
     if(common_data.socket_fd < 0)
     {
@@ -103,6 +110,15 @@ static void bind_socket(void * args)
 {
     bind_socket_struct_t * bind_socket_data = (bind_socket_struct_t *)args;
     int ret;
+
+    printf("%s\n", __func__);
+
+    unlink(bind_socket_data->socket_path);
+
+    /* configure client path */
+    strncpy(bind_socket_data->socket_address.sun_path, bind_socket_data->socket_path, sizeof(bind_socket_data->socket_address.sun_path) - 1);
+    /* configure server path */
+    strncpy(bind_socket_data->server_address.sun_path, bind_socket_data->server_path, sizeof(bind_socket_data->server_address.sun_path) - 1);
 
     ret = bind(common_data.socket_fd,                                      \
                (const struct sockaddr *)&bind_socket_data->socket_address, \
@@ -119,26 +135,34 @@ static void receive_from_socket(void * args)
 {
     (void)args;
 
-    socklen_t client_address_len = sizeof(bind_socket_data.client_address);
-    common_data.rx_byte_len = recvfrom(common_data.socket_fd,                               \
-                                       &common_data.rx_buffer[0],                           \
-                                       sizeof(&common_data.rx_buffer[0]),                   \
-                                       0,                                                   \
-                                       (struct sockaddr *)&bind_socket_data.client_address, \
-                                       &client_address_len);
+    printf("%s\n", __func__);
+
+    common_data.rx_byte_len = recvfrom(common_data.socket_fd,             \
+                                       &common_data.rx_buffer[0],         \
+                                       sizeof(common_data.rx_buffer),     \
+                                       0,                                 \
+                                       NULL,                              \
+                                       NULL);
+
+    printf("%s: %s", __func__, &common_data.rx_buffer[0]);
 }
 
 static void send_to_socket(void * args)
 {
     (void)args;
 
-    socklen_t client_address_len = sizeof(bind_socket_data.client_address);
+    printf("%s\n", __func__);
+
+    /* configure tx message */
+    strncpy(&common_data.tx_buffer[0], "Hello! I'm client\n", sizeof(common_data.tx_buffer) - 1);
+
+    socklen_t server_address_len = sizeof(bind_socket_data.server_address);
     common_data.tx_byte_len = sendto(common_data.socket_fd,                               \
-                                     &common_data.rx_buffer[0],                           \
-                                     sizeof(common_data.rx_buffer),                       \
+                                     &common_data.tx_buffer[0],                           \
+                                     sizeof(common_data.tx_buffer),                       \
                                      0,                                                   \
-                                     (struct sockaddr *)&bind_socket_data.client_address, \
-                                     client_address_len);
+                                     (struct sockaddr *)&bind_socket_data.server_address, \
+                                     server_address_len);
 }
 
 static void close_socket(void * args)
