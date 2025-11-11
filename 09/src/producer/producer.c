7a63120e598dadfd1f3379fce7bfc234b392aa77
AndersonStarling <84923856+AndersonStarling@users.noreply.github.com>
2025-11-11 14:08:32 +0700

    Update producer.c

--- 09/src/producer/producer.c
+++ 09/src/producer/producer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -24,86 +25,110 @@ int main(void)
     char * message_send = NULL;
     message_type_struct_t message;
     int mqd = -1;
+    bool loop = true;
 
-    switch(state)
+    for(; loop == true;)
     {
-        case INITIAL:
-            /* use queue to notify to another app that complete writing to share memory */
-            ret = mq_unlink(MQ_NAME);
-            if(ret < 0)
-            {
-                /* retry release program if failed */
-                perror("mq_unlink");
-            }
-            mq_attr.mq_flags = 0;
-            mq_attr.mq_maxmsg = 10;
-            mq_attr.mq_msgsize = 1024;
-            mq_attr.mq_curmsgs = 0;
-            mqd = mq_open(MQ_NAME, O_CREAT | O_RDWR, MQ_PERMISSON, &mq_attr);
-            if(mqd < 0)
-            {
-                perror("mq_open");
+        switch(state)
+        {
+            case INITIAL:
+                /* Init sequence:
+                   1. Unlink old queue
+                   2. Init queue to completing write to shared mem
+                   3. Create shared file use to store data between producer and consumer
+                   4. Allocate logical size to shared file
+                   5. Allocate shared mem and bind to shared file
+                 */
+
+                ret = mq_unlink(MQ_NAME);
+                if(ret < 0)
+                {
+                    perror("mq_unlink");
+                }
+                mq_attr.mq_flags = 0;
+                mq_attr.mq_maxmsg = 10;
+                mq_attr.mq_msgsize = sizeof(message_type_struct_t);
+                mq_attr.mq_curmsgs = 0;
+                mqd = mq_open(MQ_NAME, O_CREAT | O_RDWR, MQ_PERMISSON, &mq_attr);
+                if(mqd < 0)
+                {
+                    perror("mq_open");
+                    state = RELEASE;
+                }
+
+                fd = open(SHM_FILE_SHARED, O_CREAT | O_RDWR, 0666);
+                if(fd < 0)
+                {
+                    perror("open");
+                    state = RELEASE;
+                }
+
+                ret = ftruncate(fd, SHM_SIZE);
+                if(ret < 0)
+                {
+                    perror("ftruncate");
+                    state = RELEASE;
+                }
+
+                shared_mem_ptr = mmap(NULL, SHM_SIZE, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, 0);
+                if(MAP_FAILED == shared_mem_ptr)
+                {
+                    perror("mmap");
+                    state = RELEASE;
+                }
+
+                state = PROCESSCING;
+                break;
+            case PROCESSCING:
+                /* Processing sequence:
+                   1. Write to share mem and flush data to share file
+                   2. Send COMPLETE mesaage to consumer 
+                 */
+                ((product_info_struct_t *)shared_mem_ptr)->id = 1234;
+                ((product_info_struct_t *)shared_mem_ptr)->price = 8888;
+
+                ret = msync(shared_mem_ptr, sizeof(product_info_struct_t), MS_SYNC);
+                if(ret < 0)
+                {
+                    perror("msync");
+                    state = RELEASE;
+                }
+
+                message.task_id = getpid();
+                message.cmd = COMPLETE;
+                message_send = (char *)&message;
+                ret = mq_send(mqd, message_send, MQ_SIZE, 0);
+                if(ret < 0)
+                {
+                    perror("mq_send");
+                    state = RELEASE;
+                }
+
                 state = RELEASE;
-            }
 
-            /* init share mem */
-            fd = open("./data.txt", O_CREAT | O_RDWR, 0666);
-            if(fd < 0)
-            {
-                perror("open");
-            }
+                break;
+            case RELEASE:
+                /* Release sequence:
+                   1. Unmap shared mem
+                   2. Close queue
+                   3. Close shared file
+                 */
+                ret = munmap(shared_mem_ptr, SHM_SIZE);
+                if(ret < 0)
+                {
+                    perror("munmap");
+                }
+                mq_close(mqd);
+                close(fd);
+                loop = false;
 
-            printf("DEBUG\n");
-
-            shared_mem_ptr = mmap(NULL, SHM_SIZE, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, 0);
-
-            printf("DEBUG\n");
-            ((product_info_struct_t *)shared_mem_ptr)->id = 1234;
-            printf("DEBUG\n");
-            ((product_info_struct_t *)shared_mem_ptr)->price = 8888;
-
-            ret = msync(shared_mem_ptr, sizeof(product_info_struct_t), MS_SYNC);
-            if(ret < 0)
-            {
-                perror("msync");
-            }
-
-            state = PROCESSCING;
-            break;
-        case PROCESSCING:
-            message.task_id = getpid();
-            message.cmd = COMPLETE;
-            message_send = (char *)&message;
-            ret = mq_send(mqd, &message_send[0],    \
-                          sizeof(product_info_struct_t), 0);
-            if(ret < 0)
-            {
-                perror("mq_send");
-            }
-
-            state = RELEASE;
-
-            break;
-        case RELEASE:
-            ret = munmap(shared_mem_ptr, SHM_SIZE);
-            if(ret < 0)
-            {
-                perror("munmap");
-            }
-            ret = mq_unlink(MQ_NAME);
-            mq_close(mqd);
-            close(fd);
-            if(ret < 0)
-            {
-                /* retry release program if failed */
-                perror("mq_unlink");
-                state = RELEASE;
-            }
-            break;
-        default:
-            state = INITIAL;
-            break;
+                break;
+            default:
+                state = INITIAL;
+                break;
+        }
     }
+    
 
     return 0;
 }
