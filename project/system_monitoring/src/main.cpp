1b5ed538a01751b68a5239f813d6cfc5327f31ca
tai.nguyen <tai.nguyen24816@gmail.com>
2026-01-21 23:11:57 +0700

    update main.cpp reduce stack size of main function

--- project/system_monitoring/src/main.cpp
+++ project/system_monitoring/src/main.cpp
@@ -8,17 +8,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    model_factory f;
     view w;
 
     qDebug() << "App started!";
 
-    // cpu_info_linux * cpu   = new cpu_info_linux();
-    // disk_info_linux * disk = new disk_info_linux();
-    // mem_info_linux * mem = new mem_info_linux();
-    // system_info_linux * system = new system_info_linux();
-
-    model m(f.create_cpu(), f.create_disk(), f.create_mem(), f.create_system());
+    model_factory * f = new model_factory();
+    model m(f->create_cpu(), f->create_disk(), f->create_mem(), f->create_system());
     m.start_thread();
     controller c(&w, &m);
     w.show();
